5587d75b3b3c65b2d81b91882f76ab8dc9a3f09e
abaldeo <abaldeo@users.noreply.github.com>
2023-06-30 01:39:48 -0400

    move db tables scripts under ddl folder

--- python/create_db_tables.py
+++ python/create_db_tables.py
@@ -5,14 +5,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 DEFAULT_SCRIPTS = [
-    ("foodinspections.sqlite", "../sql/food_inspections_db_sqlite_ddl.sql"),
+    ("foodinspections.sqlite", "../sql/ddl/food_inspections_db_sqlite_ddl.sql"),
     #("foodinspections.sqlite", "../sql/orm/food_inspections_db_sqlite_pony.sql"),
-    ("foodinspections.sqlite", "../sql/food_inspections_db_create_indexes.sql"),
-    #("foodinspections.sqlite", "../sql/food_inspections_db_create_unique_indexes.sql"),
-
-    #("foodinspections.sqlite", "../sql/food_inspections_db_add_constraints.sql"),
-
-    
+    ("foodinspections.sqlite", "../sql/ddl/food_inspections_db_create_indexes.sql"),
+    #("foodinspections.sqlite", "../sql/ddl/food_inspections_db_create_unique_indexes.sql"),
+    #("foodinspections.sqlite", "../sql/ddl/food_inspections_db_add_constraints.sql"),
     ]
 
 
