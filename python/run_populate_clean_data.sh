baaff0e95c1a92d0e53d650f63ab9e6264bbd6aa
abaldeo <abaldeo@users.noreply.github.com>
2023-07-28 05:21:33 -0400

    update csv filename in cmd

--- python/run_populate_clean_data.sh
+++ python/run_populate_clean_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-poetry run python populate_models.py -db foodinspections_cleaned.sqlite -csv 05-repair_location/FoodInspection_location.csv -clean
+poetry run python populate_models.py -db foodinspections_cleaned.sqlite -csv 05-repair_location/Food_Inspections_location.csv -clean
--- python/run_populate_raw_data.sh
+++ python/run_populate_raw_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-poetry run python populate_models.py -db foodinspections_raw.sqlite -csv FoodInspection.csv
+poetry run python populate_models.py -db foodinspections_raw.sqlite -csv Food_Inspections.csv
