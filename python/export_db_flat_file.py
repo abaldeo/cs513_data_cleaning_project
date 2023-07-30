d26820d225872aa0bc0cc3c08a5e3f1f33b396bb
abaldeo <abaldeo@users.noreply.github.com>
2023-07-30 01:30:53 -0400

    fix typo

--- python/export_db_flat_file.py
+++ python/export_db_flat_file.py
@@ -5,13 +5,13 @@ import sqlite3
 
 SQL = """select  f.dba_name 'DBA Name', f.aka_name 'AKA Name',f.license_number 'License #', f.facility_type 'Facility Type', 
 l.address 'Address', l.city 'City', l.state 'State', l.zip 'Zip', l.latitude 'Latitude', l.longitude 'Longitude', l.location 'Location',
-i.inspection_id 'Inspection ID', i.inspection_type 'Inpsection Type', i.inspection_date 'Inspection Date', i.results 'Results', 
+i.inspection_id 'Inspection ID', i.inspection_type 'Inspection Type', i.inspection_date 'Inspection Date', i.results 'Results', 
 i.risk 'Risk', i.risk_category 'Risk Category', i.risk_level 'Risk Level',
 v.violation_code 'Violation Code', c.description 'Description', v.comment 'Comment'
 from FoodInspection i  
 join FoodEstablishment f  on f.id = i.establishment_id
 join EstablishmentLocation l on f.location_id = l.id 
-left join InspectionViolation v on v.inspection_id = i.inspection_id --ALWAYS USE LEFT JOIN FOR INSPECTIONVIOLATION 
+join InspectionViolation v on v.inspection_id = i.inspection_id 
 join violationcode c on c.code = v.violation_code
  """
  
